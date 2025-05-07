7fb3e290b744b38e4727efe2080676a41132c747
chandrakala24vm <chandrakalavm24@gmail.com>
2025-05-07 20:31:25 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -3,39 +3,39 @@
 ## imports
 ################################################
 ## network
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["${var.namespace}-${var.environment}-vpc"]
-  }
-}
+# data "aws_vpc" "vpc" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["${var.namespace}-${var.environment}-vpc"]
+#   }
+# }
 
 
-## network
-data "aws_subnets" "private" {
-  filter {
-    name = "tag:Name"
-    values = [
-      "${var.namespace}-${var.environment}-subnet-private1-${var.region}a",
-      "${var.namespace}-${var.environment}-subnet-private2-${var.region}b"
-    ]
-  }
+# ## network
+# data "aws_subnets" "private" {
+#   filter {
+#     name = "tag:Name"
+#     values = [
+#       "${var.namespace}-${var.environment}-subnet-private1-${var.region}a",
+#       "${var.namespace}-${var.environment}-subnet-private2-${var.region}b"
+#     ]
+#   }
 
-}
+# }
 
 
-## security
-data "aws_security_groups" "db_sg" {
-  filter {
-    name   = "group-name"
-    values = ["${var.namespace}-${var.environment}-db-sg"]
-  }
+# ## security
+# data "aws_security_groups" "db_sg" {
+#   filter {
+#     name   = "group-name"
+#     values = ["${var.namespace}-${var.environment}-db-sg"]
+#   }
 
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
-  }
-}
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.vpc.id]
+#   }
+# }
 
 
 data "aws_eks_cluster" "eks" {
